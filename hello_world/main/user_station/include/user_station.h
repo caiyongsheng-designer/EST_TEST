ce9a34fdea90ee77c39f93e92c769e571d739214
caiyongsheng-designer <804059281@qq.com>
2023-06-19 13:47:30 +0800

    错误版本，增加WiFi连接密码

--- hello_world/main/user_station/include/user_station.h
+++ hello_world/main/user_station/include/user_station.h
@@ -2,5 +2,5 @@
 #define _USER_STATION_
 
 void user_station_init();
-
+void Wifi_Connect_function();
 #endif
--- hello_world/main/user_station/user_station.c
+++ hello_world/main/user_station/user_station.c
@@ -28,7 +28,11 @@
 */
 #define EXAMPLE_ESP_WIFI_SSID      "SYC123"
 #define EXAMPLE_ESP_WIFI_PASS      "a410401663"
+#define EXAMPLE_ESP_WIFI_SSID_2    "qwert"
+#define EXAMPLE_ESP_WIFI_PASS_2      "a123456b"
 #define EXAMPLE_ESP_MAXIMUM_RETRY  10
+#define USER_WIFI_CONNECT_SUCESS   1
+#define USER_WIFI_CONNECT_FAIL     0
 
 /* FreeRTOS event group to signal when we are connected*/
 static EventGroupHandle_t s_wifi_event_group;
@@ -53,6 +57,7 @@ static void event_handler(void* arg, esp_event_base_t event_base,
             esp_wifi_connect();
             s_retry_num++;
             ESP_LOGI(TAG, "retry to connect to the AP");
+
         } else {
             xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
         }
@@ -66,23 +71,15 @@ static void event_handler(void* arg, esp_event_base_t event_base,
     }
 }
 
-void wifi_init_sta(void)
+uint8_t wifi_init_sta(wifi_config_t wifi_config)
 {
+    uint8_t user_wifi_conncet_result = 0;
     s_wifi_event_group = xEventGroupCreate();
-
-    tcpip_adapter_init();
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
-
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID,
-            .password = EXAMPLE_ESP_WIFI_PASS
-        },
-    };
 
     /* Setting a password implies station will connect to all security modes including WEP/WPA.
         * However these modes are deprecated and not advisable to be used. Incase your Access point
@@ -111,20 +108,52 @@ void wifi_init_sta(void)
     if (bits & WIFI_CONNECTED_BIT) {
         ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
                  EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+        user_wifi_conncet_result = USER_WIFI_CONNECT_SUCESS;
     } else if (bits & WIFI_FAIL_BIT) {
         ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
                  EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+        user_wifi_conncet_result = USER_WIFI_CONNECT_FAIL;
     } else {
         ESP_LOGE(TAG, "UNEXPECTED EVENT");
+        user_wifi_conncet_result = USER_WIFI_CONNECT_FAIL;
     }
 
     ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
     ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
     vEventGroupDelete(s_wifi_event_group);
+    return user_wifi_conncet_result;
 }
 
 void user_station_init()
 {
+   
+    tcpip_adapter_init();
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
-    wifi_init_sta();
+    Wifi_Connect_function();
+}
+void Wifi_Connect_function()
+{
+  wifi_config_t wifi_config_myself = {
+        .sta = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .password = EXAMPLE_ESP_WIFI_PASS
+        },
+    };
+
+    wifi_config_t wifi_config_company = {
+        .sta = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID_2,
+            .password = EXAMPLE_ESP_WIFI_PASS_2
+        },
+    };
+    if (wifi_init_sta(wifi_config_company))
+    {
+        ESP_LOGI(TAG, "WIFI connect by cpmpany");
+    }else if (wifi_init_sta(wifi_config_myself))
+    {
+        ESP_LOGI(TAG, "WIFI connect by myself");
+    }else{
+       Wifi_Connect_function();
+    }
+
 }
